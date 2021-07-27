2ea29fd7273625ed7c77b2ab83ecc4eaa3ff9459
NickPYTH <nicksrnk@gmail.com>
2021-07-27 09:33:07 +0500

    fixed created form bug

--- VoteAppDN/create_vote/models.py
+++ VoteAppDN/create_vote/models.py
@@ -8,7 +8,7 @@ class Form(models.Model):
         max_length=300,
         validators=[
             validators.MinLengthValidator(3, message="Слишком короткое имя формы"),
-            validators.MaxLengthValidator(300, message="Слишком длинное имя формы"),
+            validators.MaxLengthValidator(500, message="Слишком длинное имя формы"),
         ],
         unique=True,
         verbose_name="Название формы",
@@ -18,9 +18,6 @@ class Form(models.Model):
         validators=[
             validators.MinLengthValidator(
                 4, message="Слишком короткий пароль к результатам"
-            ),
-            validators.MaxLengthValidator(
-                20, message="Слишком длинный пароль к результатам"
             ),
         ],
         verbose_name="Пароль к результатам",
@@ -42,22 +39,22 @@ class Question(models.Model):
     question_type = models.CharField(verbose_name="Тип вопроса", max_length=20)
     form = models.ForeignKey(Form, on_delete=models.CASCADE)
     question_name = models.CharField(
-        max_length=40,
+        max_length=500,
         validators=[
             validators.MinLengthValidator(
                 4, message="Слишком короткий заголвок вопроса"
             ),
             validators.MaxLengthValidator(
-                40, message="Слишком длинный заголовок вопроса"
+                500, message="Слишком длинный заголовок вопроса"
             ),
         ],
         verbose_name="Заголовок вопроса",
     )
     question_description = models.CharField(
-        max_length=100,
+        max_length=500,
         validators=[
             validators.MaxLengthValidator(
-                100, message="Слишком длинное описание вопроса"
+                500, message="Слишком длинное описание вопроса"
             ),
         ],
         verbose_name="Описание вопроса",
@@ -74,22 +71,22 @@ class Answer(models.Model):
         on_delete=models.CASCADE,
         verbose_name="Вопрос")
     answer = models.CharField(
-        max_length=40,
+        max_length=500,
         verbose_name="Ответ")
     group = models.CharField(
-        max_length=40,
+        max_length=500,
         verbose_name="Группа",
         blank=True,
         null=True)
 
 class SendedForm(models.Model):
     form_key = models.CharField(max_length=50, verbose_name="Ключ формы")
-    question = models.CharField(max_length=50, verbose_name="Вопрос")
-    answer = models.CharField(max_length=50, verbose_name="Ответ")
+    question = models.CharField(max_length=500, verbose_name="Вопрос")
+    answer = models.CharField(max_length=500, verbose_name="Ответ")
     date = models.DateTimeField(auto_now_add=True, editable=False)
 
 class SendedComments(models.Model):
     form_key = models.CharField(max_length=50, verbose_name="Ключ формы")
-    question = models.CharField(max_length=50, verbose_name="Вопрос")
-    comment = models.CharField(max_length=50, verbose_name="Комментарий")
+    question = models.CharField(max_length=500, verbose_name="Вопрос")
+    comment = models.CharField(max_length=1000, verbose_name="Комментарий")
     date = models.DateTimeField(auto_now_add=True, editable=False)
